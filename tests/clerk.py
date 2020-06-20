11e90e9c734165c16bf80a11a78cca8d74e57e32
hundredvisionsguy <cwinikka@gmail.com>
2020-06-20 13:37:47 -0700

    fixing file_to_string and adding get_css

--- tests/clerk.py
+++ tests/clerk.py
@@ -1,10 +1,12 @@
 # clerk.py
-
 import io 
 import os.path
+import AdvancedHTMLParser as h_parser
 
 def file_to_string(path):
-  f = open(path, "r")
+  THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+  my_file = os.path.join(THIS_FOLDER, path)
+  f = open(my_file, "r")
   file = f.read()
   file = file.replace('\n','')
   file = file.replace('  ', '')
@@ -13,3 +15,20 @@ def file_to_string(path):
 def get_file_type(path):
   extension = os.path.splitext(path)[1]
   return extension [1:]
+
+def get_css(path):
+  css = ''
+  full_code = file_to_string(path)
+  parser = h_parser.AdvancedHTMLParser()
+  parser.parseStr(full_code)
+  css_advancedTag = parser.getElementsByTagName('style')
+  css = css_advancedTag[0].innerText
+  return css
+
+if __name__ == "__main__":
+  html_with_css = "test_files/html_with_css.html"
+  extension = get_file_type(html_with_css)
+  type(extension)
+  code_string = file_to_string(html_with_css)
+  css_code = get_css(html_with_css)
+  type(css_code)
--- tests/test_clerk.py
+++ tests/test_clerk.py
@@ -1,8 +1,10 @@
 import pytest
 import clerk 
 
-css_file_path = 'test_files/test.css'
+css_file_path =  'test_files/test.css'
 html_file_path = 'test_files/sample_no_errors.html'
+html_with_css =  "test_files/html_with_css.html"
+sample_txt_path = 'test_files/sample.txt'
 
 def test_get_file_type_for_html():
   filetype = clerk.get_file_type(html_file_path)
@@ -11,3 +13,13 @@ def test_get_file_type_for_html():
 def test_get_file_type_for_css():
   filetype = clerk.get_file_type(css_file_path)
   assert filetype == 'css'
+
+def test_get_css_from_style():
+  css_code = clerk.get_css(html_with_css)
+  expected = "p {color:red;}"
+  assert css_code == expected
+
+def test_file_to_string_with_sample():
+  sample_test = clerk.file_to_string(sample_txt_path)
+  expected = "Hey!"
+  assert sample_test == expected
