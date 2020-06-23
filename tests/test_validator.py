31ea33be954975bc5e6cbeaa61eb3529adb65c8d
hundredvisionsguy <cwinikka@gmail.com>
2020-06-23 13:03:38 -0700

    adding css validity and tests

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -12,9 +12,17 @@ def test_no_html_validator_errors(filename):
   num_errors = len(no_errors)
   assert num_errors == 0
 
-# type(no_errors)
-# print("There are {} errors.".format(val.get_num_errors(no_errors)))
+def test_get_css_validity_with_errors():
+  error_str = "p } color: blue;{"
+  is_valid = val.is_css_str_valid(error_str)
+  assert is_valid == False
 
-# with_errors = val.get_markup_validity('tests/test_files/sample_with_errors.html')
-# for i in with_errors:
-#   print(i)
+def test_get_css_validity_with_no_errors():
+  code = "p { color: #336600; }"
+  is_valid = val.is_css_str_valid(code)
+  assert is_valid == True
+
+def test_get_css_errors_no_start_curley():
+  css_error = val.get_css_errors('p } color: #336699; }')
+  expected = "CSSStyleRule: No start { of style declaration found: 'p } color: #336699; }' [1:22: ]"
+  assert css_error == expected
--- tests/validator.py
+++ tests/validator.py
@@ -6,6 +6,12 @@ import certifi
 import io
 import json
 import os
+import clerk
+import cssutils
+import subprocess
+import sys
+
+parser = cssutils.CSSParser(raiseExceptions=SyntaxError)
 
 def get_markup_validity(filepath):
   w3cURL = 'https://validator.w3.org/nu/?out=json'
@@ -16,6 +22,20 @@ def get_markup_validity(filepath):
     r = requests.post(w3cURL, data=payload, headers=headers)
     errors = r.json()['messages']
   return errors
+
+def is_css_str_valid(str_code):
+  try:
+    parser.parseString(str_code) 
+    return True
+  except:
+    return False
+
+def get_css_errors(str_code):
+  try:
+    parser.parseString(str_code)
+    return ""
+  except Exception as e:
+    return str(e)
 
 def get_num_errors(report):
   num = len(report)
@@ -44,3 +64,10 @@ if __name__ == "__main__":
   
   # test getting number of errors
   print(get_num_errors(report))
+  
+  css_code = clerk.get_css('test_files/html_with_css.html')
+  is_css_valid = is_css_str_valid(css_code)
+  print(is_css_valid)
+  is_css_valid = is_css_str_valid("p } color: #336699; }")
+  print(is_css_valid)
+  print(get_css_errors("#header { display: flex; }"))
