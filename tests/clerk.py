b50e9c9f1eafa226564fabc12bc374e5374dc594
hundredvisionsguy <cwinikka@gmail.com>
2020-06-25 12:12:31 -0700

    replacing os with pathlib in clerk

--- tests/clerk.py
+++ tests/clerk.py
@@ -1,34 +1,48 @@
 # clerk.py
 import io 
-import os.path
 import AdvancedHTMLParser as h_parser
+from pathlib import Path
+
+working_dir = Path.cwd()
+
+def get_path_list(path):
+  path_list = path.split('/')
+  return path_list
+
+def get_full_path_string(path):
+  """path must be a relative path starting with working directory """
+  full_path = working_dir
+  p_list = get_path_list(path)
+  for i in p_list:
+    full_path = full_path / i
+  return full_path
 
 def file_to_string(path):
-  THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-  my_file = os.path.join(THIS_FOLDER, path)
-  f = open(my_file, "r")
-  file = f.read()
-  file = file.replace('\n','')
-  file = file.replace('  ', '')
+  my_file = get_full_path_string(path) 
+  file = my_file.read_text()
   return file 
 
 def get_file_type(path):
-  extension = os.path.splitext(path)[1]
-  return extension [1:]
+  my_file = get_full_path_string(path)
+  suffix = my_file.suffix
+  return suffix[1:]
 
-def get_css(path):
-  css = ''
+def get_css_from_style_tag(path):
   full_code = file_to_string(path)
   parser = h_parser.AdvancedHTMLParser()
   parser.parseStr(full_code)
   css_advancedTag = parser.getElementsByTagName('style')
-  css = css_advancedTag[0].innerText
-  return css
+  return css_advancedTag[0].innerText
+
+def get_all_project_files():
+  return []
 
 if __name__ == "__main__":
-  html_with_css = "test_files/html_with_css.html"
+  html_with_css = "tests/test_files/html_with_css.html"
+  p_list = get_full_path_string(html_with_css)
   extension = get_file_type(html_with_css)
-  type(extension)
+  print("the extension for {} is {}".format(html_with_css, extension))
   code_string = file_to_string(html_with_css)
-  css_code = get_css(html_with_css)
-  type(css_code)
+  print(code_string)
+  css_code = get_css_from_style_tag(html_with_css)
+  print("the type of the CSS code is {}".format(type(css_code)))
--- tests/test_clerk.py
+++ tests/test_clerk.py
@@ -1,10 +1,12 @@
 import pytest
 import clerk 
 
-css_file_path =  'test_files/test.css'
-html_file_path = 'test_files/sample_no_errors.html'
-html_with_css =  "test_files/html_with_css.html"
-sample_txt_path = 'test_files/sample.txt'
+css_file_path =  'tests/test_files/test.css'
+html_file_path = 'tests/test_files/sample_no_errors.html'
+html_with_css =  "tests/test_files/html_with_css.html"
+sample_txt_path = 'tests/test_files/sample.txt'
+working_dir_txt_path = 'sample.txt'
+project_test_css_path = 'project/test.css'
 
 def test_get_file_type_for_html():
   filetype = clerk.get_file_type(html_file_path)
@@ -15,7 +17,7 @@ def test_get_file_type_for_css():
   assert filetype == 'css'
 
 def test_get_css_from_style():
-  css_code = clerk.get_css(html_with_css)
+  css_code = clerk.get_css_from_style_tag(html_with_css)
   expected = "p {color:red;}"
   assert css_code == expected
 
@@ -23,3 +25,13 @@ def test_file_to_string_with_sample():
   sample_test = clerk.file_to_string(sample_txt_path)
   expected = "Hey!"
   assert sample_test == expected
+
+def test_file_to_string_in_working_directory():
+  sample_text = clerk.file_to_string(working_dir_txt_path)
+  expected = "Sample text from working directory"
+  assert sample_text == expected
+
+def test_file_to_string_in_project_directory():
+  sample_text = clerk.file_to_string(project_test_css_path)
+  expected = "p { display: flex; color: fred; }"
+  assert sample_text == expected
