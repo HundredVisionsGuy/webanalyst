61d782ebbdd5860bdeab40a64d921dd270ed7070
hundredvisionsguy <cwinikka@gmail.com>
2020-06-25 18:02:35 -0700

    adding get_all_project_files() and def get_all_files_of_type(dir, filetype):  to clerk.py

--- tests/clerk.py
+++ tests/clerk.py
@@ -2,6 +2,7 @@
 import io 
 import AdvancedHTMLParser as h_parser
 from pathlib import Path
+import collections
 
 working_dir = Path.cwd()
 
@@ -34,15 +35,45 @@ def get_css_from_style_tag(path):
   css_advancedTag = parser.getElementsByTagName('style')
   return css_advancedTag[0].innerText
 
-def get_all_project_files():
-  return []
+def get_all_project_files(dir):
+  files = []
+  files += get_all_files_of_type(dir, 'html')
+  files += get_all_files_of_type(dir, 'css')
+  files += get_all_files_of_type(dir, 'js')
+  return files
+
+def get_all_files_of_type(dir, filetype):
+  pattern = "*." + filetype + "*"
+  output = []
+  files = collections.Counter(str(f) for f in Path(dir).rglob(pattern))
+  output += files.keys()
+  return output
 
 if __name__ == "__main__":
   html_with_css = "tests/test_files/html_with_css.html"
+
+  # get full path of a relative link
   p_list = get_full_path_string(html_with_css)
+  
+  # get the extension of a file
   extension = get_file_type(html_with_css)
   print("the extension for {} is {}".format(html_with_css, extension))
+
+  # get all the code from a file as a string
   code_string = file_to_string(html_with_css)
   print(code_string)
+
+  # extract CSS code from the style tag
   css_code = get_css_from_style_tag(html_with_css)
-  print("the type of the CSS code is {}".format(type(css_code)))
+
+  # test get_all_project_files()
+  test_project_files = 'project'
+  results = get_all_project_files(test_project_files)
+  for i in results:
+    print(i)
+
+  # test getting list of all files with .css extension
+  test_project_files = 'tests/test_files/project'
+  results = get_all_files_of_type(test_project_files, '*.css*')
+  for i in results:
+    print(i)
--- tests/test_clerk.py
+++ tests/test_clerk.py
@@ -35,3 +35,19 @@ def test_file_to_string_in_project_directory():
   sample_text = clerk.file_to_string(project_test_css_path)
   expected = "p { display: flex; color: fred; }"
   assert sample_text == expected
+
+def test_get_all_project_files_from_test_project():
+  expected = ['tests\\test_files\\project\\gallery.html',
+              'tests\\test_files\\project\\index.html',
+              'tests\\test_files\\project\\css\\general.css',
+              'tests\\test_files\\project\\css\\grid-layout.css',
+              'tests\\test_files\\project\\css\\layout.css',
+              'tests\\test_files\\project\\css\\navigation.css',
+              'tests\\test_files\\project\\js\\scripts.js']
+  results = clerk.get_all_project_files('tests/test_files/project')
+  assert expected == results
+def test_get_all_html_project_files_from_test_project():
+  expected = ['tests\\test_files\\project\\gallery.html',
+    'tests\\test_files\\project\\index.html']
+  results = clerk.get_all_files_of_type('tests/test_files/project', 'html')
+  assert expected == results
