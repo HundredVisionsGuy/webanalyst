937c734f31d4bf15cb823075f7538a41021f64bc
hundredvisionsguy <cwinikka@gmail.com>
2020-08-03 18:09:09 -0700

    refactoring test_report for about_me project to make tests more clear as to which project is being tested

--- tests/test_report.py
+++ tests/test_report.py
@@ -7,13 +7,13 @@ import report
 import clerk
 from bs4 import BeautifulSoup
 
-about_me_dir_path = "tests/test_files/projects/about_me/"
+about_me_path = "tests/test_files/projects/about_me/"
 large_project_path = "tests/test_files/projects/large_project/"
 
 
 @pytest.fixture
-def my_report():
-    my_report = report.Report(about_me_dir_path)
+def about_me_report():
+    my_report = report.Report(about_me_path)
     return my_report
 
 
@@ -24,8 +24,8 @@ def my_large_project_report():
 
 
 @pytest.fixture
-def report_readme_list():
-    my_report = report.Report(about_me_dir_path)
+def about_me_readme_list():
+    my_report = report.Report(about_me_path)
     my_list = my_report.get_readme_list()
     return my_list
 
@@ -38,9 +38,9 @@ def large_project_readme_list():
 
 
 @pytest.fixture
-def my_general_report(report_readme_list):
+def about_me_general_report(about_me_readme_list):
     general_report = report.GeneralReport(
-        report_readme_list, about_me_dir_path)
+        about_me_readme_list, about_me_path)
     general_report.generate_report()
     return general_report
 
@@ -54,8 +54,8 @@ def my_large_project_general_report(large_project_readme_list):
 
 
 @pytest.fixture
-def my_html_report(report_readme_list):
-    html_report = report.HTMLReport(report_readme_list, about_me_dir_path)
+def about_me_html_report(about_me_readme_list):
+    html_report = report.HTMLReport(about_me_readme_list, about_me_path)
     return html_report
 
 
@@ -67,8 +67,8 @@ def my_large_project_html_report(large_project_readme_list):
 
 
 @pytest.fixture
-def my_css_report(report_readme_list):
-    css_report = report.CSSReport(report_readme_list, about_me_dir_path)
+def about_me_css_report(about_me_readme_list):
+    css_report = report.CSSReport(about_me_readme_list, about_me_path)
     return css_report
 
 
@@ -86,61 +86,61 @@ def paragraph_tag():
     return p_tag
 
 
-def test_report_for_report_object(my_report):
-    assert my_report
+def test_about_me_report_for_report_object(about_me_report):
+    assert about_me_report
 
 
-def test_get_readme_text_for_about_me(my_report):
-    results = my_report.get_readme_text()[:24]
+def test_get_readme_text_for_about_me(about_me_report):
+    results = about_me_report.get_readme_text()[:24]
     expected = "# Project Name: About Me"
     assert results == expected
 
 
-def test_general_report_for_report_title_from_about_me(my_general_report):
-    my_general_report.set_title()
-    results = my_general_report.get_title()
+def test_about_me_general_report_for_title(about_me_general_report):
+    about_me_general_report.set_title()
+    results = about_me_general_report.get_title()
     expected = "About Me"
     assert results == expected
 
 
-def test_general_report_for_get_description_for_about_me(my_general_report):
-    my_general_report.set_description()
-    results = my_general_report.get_description()
+def test_about_me_report_for_get_description(about_me_general_report):
+    about_me_general_report.set_description()
+    results = about_me_general_report.get_description()
     expected = """Students are asked to create a single web page designed to give information about the student using Headings (h1 & h2), paragraphs, links, and bold and italics."""
     assert results == expected
 
 
-def test_html_report_for_get_html_level(my_html_report):
-    results = my_html_report.get_html_level()
+def test_about_me_html_report_for_get_html_level(about_me_html_report):
+    results = about_me_html_report.get_html_level()
     expected = "101"
     assert results == expected
 
 
-def test_report_for_about_me_wordcount(my_general_report):
-    results = my_general_report.get_word_count()
+def test_about_me_report_for_wordcount(about_me_general_report):
+    results = about_me_general_report.get_word_count()
     expected = 61
     assert results == expected
 
 
-def test_general_report_get_num_words(my_general_report, paragraph_tag):
-    results = my_general_report.get_num_words(paragraph_tag)
+def test_about_me_general_report_for_get_num_words(about_me_general_report, paragraph_tag):
+    results = about_me_general_report.get_num_words(paragraph_tag)
     expected = 22
     assert results == expected
 
 
-def test_html_report_for_num_of_files(my_html_report):
-    results = my_html_report.get_num_html_files()
+def test_about_me_html_report_for_num_of_files(about_me_html_report):
+    results = about_me_html_report.get_num_html_files()
     expected = 1
     assert results == expected
 
 
-def test_css_report_for_num_of_files(my_css_report):
-    results = my_css_report.get_num_css_files()
+def test_about_me_css_report_for_num_of_files(about_me_css_report):
+    results = about_me_css_report.get_num_css_files()
     expected = 0
     assert results == expected
 
 
-def test_css_report_for_num_style_tags_1(my_css_report):
-    results = my_css_report.get_num_style_tags()
+def test_about_me_css_report_for_num_style_tags_1(about_me_css_report):
+    results = about_me_css_report.get_num_style_tags()
     expected = 1
     assert results == expected
