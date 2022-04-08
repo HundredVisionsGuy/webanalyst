5494e7c8d883ae5f5ae5fa37c07da9d5a0bc0075
hundredvisionsguy <cwinikka@gmail.com>
2022-04-08 10:46:17 -0700

    fixing the HTML required head bug - no longer counts header tag

--- webanalyst/CSSReport.py
+++ webanalyst/CSSReport.py
@@ -902,9 +902,10 @@ class CSSReport:
     def get_families(self, declaration):
         families = []
         for ruleset in declaration.rulesets:
-            for declaration in ruleset.declaration_block.declarations:
-                if declaration.property in ("font", "font-family"):
-                    families.append(declaration.value)
+            if ruleset.declaration_block:
+                for declaration in ruleset.declaration_block.declarations:
+                    if declaration.property in ("font", "font-family"):
+                        families.append(declaration.value)
         return families
 
 
@@ -1071,7 +1072,12 @@ class CSSReport:
                     declaration_blocks[declaration_block] = [source, ]
         for sheet in self.stylesheet_objects:
             for ruleset in sheet.rulesets:
-                declaration_block = "{" + ruleset.declaration_block.text
+                # it's possible someone places html, so only process
+                # if the declaration block is NOT a None type
+                if ruleset.declaration_block:
+                    declaration_block = "{" + ruleset.declaration_block.text
+                else:
+                    continue
                 source = sheet.href
                 try:
                     if declaration_blocks[declaration_block]:
